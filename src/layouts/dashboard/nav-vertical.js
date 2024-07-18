5aecc9618474cd72d563f5ddb888d995562f2baa
shakirzedsofttech <shakirzedsoft@gmail.com>
2024-07-18 11:38:56 +0530

    modify1

--- src/layouts/dashboard/nav-vertical.js
+++ src/layouts/dashboard/nav-vertical.js
@@ -58,7 +58,8 @@ export default function NavVertical({ openNav, onCloseNav }) {
 
       <Box sx={{ flexGrow: 1 }} />
 
-      <NavUpgrade />
+      {/* <NavUpgrade /> */}
+      
     </Scrollbar>
   );
 
