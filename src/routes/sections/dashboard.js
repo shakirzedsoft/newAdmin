530ceab7be94d4829465ef26285ae3e35aaf4513
Aswathicp710 <aswathy.zedsoft@gmail.com>
2024-08-07 10:23:18 -0700

    real estate

--- src/routes/sections/dashboard.js
+++ src/routes/sections/dashboard.js
@@ -18,6 +18,7 @@ const OverviewBookingPage = lazy(() => import('src/pages/dashboard/booking'));
 const OverviewFilePage = lazy(() => import('src/pages/dashboard/file'));
 
 
+
 //zedSoft
 //-------------------//
 
